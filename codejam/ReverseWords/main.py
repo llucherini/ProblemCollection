523eef92aee992bc47f11e64740c0ae4a92d93a9
Lorenzo Lucherini <lorenzo.lucherini@gmail.com>
2013-01-31 01:21:09 +0100

    Solved the Reverse Words problem from Google Code Jam http://code.google.com/codejam/contest/351101/dashboard

---
+++ codejam/ReverseWords/main.py
@@ -0,0 +1,13 @@
+# Google Code Jam - Reverse Words
+# http://code.google.com/codejam/contest/351101/dashboard
+
+# Some plumbing to parse the input files from stdin
+if __name__ == "__main__":
+    import sys
+    
+    n_tests = int(sys.stdin.readline().strip())
+    for n in xrange(n_tests):
+        words = sys.stdin.readline().strip().split(' ')
+
+        print "Case #%d: %s" % (n+1, ' '.join(reversed(words)))
+        
